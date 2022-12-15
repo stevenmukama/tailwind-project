5c0020164658e2dcc4ecf7f78b1e2bbf2c12b956
Mukama Steven <stevenmukama1@gmail.com>
2022-12-15 13:35:05 +0200

    Added tailwind css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className='text-center text-green-500 font-bold'>
+      <p>good morning!</p>
+    </div>
+  );
+}
+
+export default App;
